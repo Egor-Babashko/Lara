4a5c18852615258160abb269df96ae181cf817f0
Hash <egor.babashko2005@mail.ru>
2022-01-20 23:43:09 +0300

    Output and addition of articles

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Article;
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    public function show(): \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Contracts\Foundation\Application
+    {
+        return view('pages.both.article');
+    }
+
+    public function showCreate() {
+        return view('pages.user.create_article');
+    }
+
+    public function store(Request $request) {
+        $data = $request->post();
+        dump($data);
+
+        $validator = validator($data, [
+           'title' => 'required',
+           'content' => 'required',
+        ]);
+
+        if($validator->fails()) {
+            dump($validator->errors());
+        } else {
+            Article::create();
+        }
+
+        Article::create($data);
+    }
+}
---
+++ database/migrations/2022_01_20_153339_create_articles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->longText("content");
+            $table->string("title");
+            $table->integer("likes", "default", 0);
+            $table->integer("dis_likes", "default", 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
